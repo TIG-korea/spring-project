17554465c036611a99eb8ee5978e9b0e264d6bc7
hongsoonchan02 <asm640789@gmail.com>
2026-02-20 17:19:21 +0900

    feat<header.html> : 공통 헤더 기능 추가

--- src/main/java/kr/co/spring_project/board/controller/BoardController.java
+++ src/main/java/kr/co/spring_project/board/controller/BoardController.java
@@ -1,5 +1,19 @@
 package kr.co.spring_project.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+	
+	@GetMapping("/question/form")
+	public String questionForm() {
+		return "/form";
+	}
 
 }
